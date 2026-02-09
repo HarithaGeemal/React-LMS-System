60a9735abd643fb7fe5df4fc902b53cfaf6b8d46
Haritha Geemal <geemalharitha631@gmail.com>
2026-02-10 00:06:51 +0530

    Hero welcome section completed

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -3,7 +3,7 @@ import logo_dark from './logo_dark.svg'
 import search_icon from './search_icon.svg'
 import cross_icon from './cross_icon.svg'
 import upload_area from './upload_area.svg'
-import sketch from './sktech.svg'
+import sketch from './sketch.svg'
 import microsoft_logo from './microsoft_logo.svg'
 import walmart_logo from './walmart_logo.svg'
 import accenture_logo from './accenture_logo.svg'
@@ -42,6 +42,7 @@ import profile_img from './profile_img.png'
 import profile_img2 from './profile_img2.png'
 import profile_img3 from './profile_img3.png'
 import lesson_icon from './lesson_icon.svg'
+import skilloralogo from './skillora-logo.png'
 
 
 export const assets = {
@@ -88,7 +89,8 @@ export const assets = {
     profile_img3,
     play_icon,
     blue_tick_icon,
-    lesson_icon
+    lesson_icon,
+    skilloralogo
 }
 
 export const dummyEducatorData = {
--- frontend/src/components/student/Navbar.jsx
+++ frontend/src/components/student/Navbar.jsx
@@ -13,18 +13,22 @@ const Navbar = () => {
     return (
 
         <div className={`flex w-full items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-200 py-4 shadow-sm sticky top-0 z-50 backdrop-blur-md ${isCourseListPage ? 'bg-white' : 'bg-cyan-100/70'}`} >
-            <img src={assets.logo} alt="Logo" className="w-28 lg:w-32
+            <img src={assets.skilloralogo} alt="Logo" className="w-42 lg:w-52
             cursor-pointer hover:scale-105 transition-transform duration-200"/>
 
             <div className="hidden md:flex items-center gap-6 text-gray-600">
                 <div className="flex items-center gap-4">
-                    <button className="text-base font-medium hover:text-blue-600 transition-colors duration-200 cursor-pointer">
-                        Become Educator
-                    </button>
+                    {user &&
+                        <>
+                            <button className="text-base font-medium hover:text-blue-600 transition-colors duration-200 cursor-pointer">
+                                Become Educator
+                            </button>
 
-                    <div className="h-5 w-[2px] bg-blue-400"></div>
+                            <div className="h-5 w-[2px] bg-blue-400"></div>
 
-                    <Link to='/my-enrollments' className="text-base font-medium hover:text-blue-600 transition-colors duration-200">My Enrollments</Link>
+                            <Link to='/my-enrollments' className="text-base font-medium hover:text-blue-600 transition-colors duration-200">My Enrollments</Link>
+                        </>
+                    }
                 </div>
 
                 {user ? <UserButton /> :
@@ -36,11 +40,15 @@ const Navbar = () => {
             {/* mobile screen view */}
 
             <div className="md:hidden flex items-center gap-6 text-gray-600">
-                <div className="flex items-center gap-8">
-                    <button className="hover:text-blue-600 transition cursor-pointer">Become Educator</button>
-                    <div className="h-4 w-[2px] bg-gray-500 opacity-70"></div>
+                <div className="flex items-center gap-1 sm:gap-2 max-sm:text-xs">
+                    {user &&
+                        <>
+                            <button className="hover:text-blue-600 transition cursor-pointer">Become Educator</button>
+                            <div className="h-4 w-[2px] bg-gray-500 opacity-70"></div>
 
-                    <Link to='/my-enrollments' className="hover:text-blue-600 transition">My Enrollments</Link>
+                            <Link to='/my-enrollments' className="hover:text-blue-600 transition">My Enrollments</Link>
+                        </>
+                    }
                 </div>
 
                 {user ? <UserButton /> :
