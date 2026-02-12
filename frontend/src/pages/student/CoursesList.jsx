c26e8f22030e343cdedd4b8280c2fe8794e9b8e3
Haritha Geemal <geemalharitha631@gmail.com>
2026-02-12 23:11:01 +0530

    Filter option added

--- frontend/src/pages/student/CoursesList.jsx
+++ frontend/src/pages/student/CoursesList.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { useContext } from 'react';
+import React, { useEffect } from 'react'
+import { useContext, useState } from 'react';
 import { AppContext } from '../../context/AppContext';
 import SearchBar from "../../components/student/SearchBar";
 import CourseCard from "../../components/student/CourseCard"
@@ -9,6 +9,23 @@ const CoursesList = () => {
 
     const { navigate, allCourses } = useContext(AppContext);
     const { input } = useParams()
+
+    const [filteredCourses, setFilteredCourses] = useState([])
+
+    useEffect(() =>{
+        if(allCourses && allCourses.length > 0){
+            const tempCourses = allCourses.slice();
+
+            input ? 
+            setFilteredCourses(
+                tempCourses.filter(
+                    item  => item.courseTitle.toLowerCase().includes(input.toLowerCase())
+                )
+            )
+            :setFilteredCourses(tempCourses)
+
+        }
+    },[allCourses, input])
 
     return (
         <>
@@ -26,7 +43,7 @@ const CoursesList = () => {
                     </div>
                 </div>
                 <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 my-16 gap-3 px-2 md:p-0'>
-                    {allCourses.map((course, index) => <CourseCard key={index} course={course} />)}
+                    {filteredCourses.map((course, index) => <CourseCard key={index} course={course} />)}
                 </div>
             </div>
         </>
