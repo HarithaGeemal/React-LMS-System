a56e9136de99a3cbf5dc87b25615008055451d48
Haritha Geemal <geemalharitha631@gmail.com>
2026-02-15 02:49:36 +0530

    Completed Rating feature and player page (Student part is completed)

--- frontend/src/components/student/Rating.jsx
+++ frontend/src/components/student/Rating.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const Rating = () => {
+const Rating = ({initialRating , onRate}) => {
+
+    const [rating,setrating] = useState( initialRating ||0);
+
+    const handleRating = (value) => {
+        setrating(value);
+        if(onRate) onRate(value)
+    }
+
+    useEffect(()=>{
+        if(initialRating){
+            setrating(initialRating)
+        }
+    },[initialRating])
+
     return (
         <div>
-            <h1>Rating</h1>
+            {Array.from({length:5}, (_, index)=>{
+                const startValue = index + 1;
+                return (
+                    <span key={index} className={`text-xl sm:text-2xl cursor-pointer transition-colors ${startValue <= rating ? 'text-yellow-500' : 'text-gray-300'}`}
+                    onClick={() => handleRating(startValue)}>
+                        &#9733;
+                    </span>
+                )
+            })}
         </div>
     )
 }
+
 
 export default Rating
