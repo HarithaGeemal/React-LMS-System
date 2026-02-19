1438dc1a1c39a50dddea736b837ca70afa808f2c
Haritha Geemal <geemalharitha631@gmail.com>
2026-02-20 00:03:43 +0530

    Stripe payment gateway fixed

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/mongodb.js";
-import { clerkWebhook } from "./controllers/webhooks.js";
+import { clerkWebhook , stripeWebhook} from "./controllers/webhooks.js";
 import educatorRouter from "./routes/educatorRoutes.js";
 import { clerkMiddleware } from "@clerk/express";
 import connectCloudinary from "./configs/cloudinary.js";
@@ -30,5 +30,6 @@ app.post('/clerk',express.json(),clerkWebhook);
 app.use('/api/educator', express.json() , educatorRouter);
 app.use('/api/course', express.json() , courseRouter);
 app.use('/api/user', express.json() , userRouter);
+app.post('/stripe',express.raw({type:'application/json'}),stripeWebhook);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
