948772b9a574b61b453716b7392386cc4d991b69
Haritha Geemal <geemalharitha631@gmail.com>
2026-02-22 00:40:58 +0530

    bug fixed

--- backend/server.js
+++ backend/server.js
@@ -16,25 +16,13 @@ const PORT = process.env.PORT || 5000;
 await connectDB();
 connectCloudinary();
 
-const allowedOrigins = [
-    "http://localhost:5173",
-    "https://react-lms-system.vercel.app", // Vercel frontend
-];
-
 // middlewares
 
 app.use(cors({
-    origin: (origin, cb) => {
-        // allow requests with no origin (like server-to-server, webhooks, Postman)
-        if (!origin) return cb(null, true);
-        if (allowedOrigins.includes(origin)) return cb(null, true);
-        return cb(new Error(`CORS blocked for origin: ${origin}`));
-    },
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    origin: ["http://localhost:5173", "https://react-lms-system.vercel.app"],
+    // frontend
+    credentials: true
 }));
-app.options("*", cors());
 app.use(clerkMiddleware());
 
 
